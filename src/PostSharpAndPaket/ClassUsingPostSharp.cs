f7fd0cefe334b533efef02554a095457e22953e5
Marko Apfel <marko.apfel@de.bosch.com>
2019-11-08 17:35:16 +0100

    add a test for the PostSharp functionality

--- src/PostSharpAndPaket/ClassUsingPostSharp.cs
+++ src/PostSharpAndPaket/ClassUsingPostSharp.cs
@@ -1,4 +1,6 @@
 ﻿using PostSharp.Patterns.Contracts;
+using System;
+using Xunit;
 
 namespace PostSharpAndPaket
 {
@@ -6,5 +8,24 @@ namespace PostSharpAndPaket
     {
         [Range(1, 7)]
         public int Counter { get; set; }
+    }
+
+    public class ClassUsingPostSharpTest
+    {
+        [Fact]
+        public void Verify_That_The_RangeAttribute_Is_Included()
+        {
+            var sut = new ClassUsingPostSharp();
+
+            var exception1 = Record.Exception(() => sut.Counter = 0);
+            Assert.NotNull(exception1);
+            Assert.IsType<ArgumentOutOfRangeException>(exception1);
+            Assert.StartsWith($"The property 'Counter' must be between 1 and 7.", exception1.Message);
+
+            var exception2 = Record.Exception(() => sut.Counter = 8);
+            Assert.NotNull(exception2);
+            Assert.IsType<ArgumentOutOfRangeException>(exception2);
+            Assert.StartsWith($"The property 'Counter' must be between 1 and 7.", exception2.Message);
+        }
     }
 }
