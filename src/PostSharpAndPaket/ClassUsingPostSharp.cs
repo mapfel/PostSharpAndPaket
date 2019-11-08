ed4e15a48394700907808600e3af4941582cb27f
Marko Apfel <marko.apfel@de.bosch.com>
2019-11-08 17:26:20 +0100

    add property with Range attribute

--- src/PostSharpAndPaket/ClassUsingPostSharp.cs
+++ src/PostSharpAndPaket/ClassUsingPostSharp.cs
@@ -1,6 +1,10 @@
-﻿namespace PostSharpAndPaket
+﻿using PostSharp.Patterns.Contracts;
+
+namespace PostSharpAndPaket
 {
     public class ClassUsingPostSharp
     {
+        [Range(1, 7)]
+        public int Counter { get; set; }
     }
 }
